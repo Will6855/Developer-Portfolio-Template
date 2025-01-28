1d565afc870ed695ce52020c23387217faf667f8
Will <86987398+Will6855@users.noreply.github.com>
2025-01-28 23:40:30 +0100

    refactor: Consolidate portfolio data in PortfolioService and update related files
    - Moved portfolio-related data to PortfolioService.php
    - Updated templates to reflect the new data structure
    - Reorganized translation files (messages.en.yaml and messages.fr.yaml)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
+use App\Service\PortfolioService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\ContactType;
 use Symfony\Component\Mailer\MailerInterface;
@@ -14,11 +14,12 @@ use Symfony\Component\Mime\Address;
 
 class HomeController extends AbstractController
 {
-    private TranslatorInterface $translator;
 
-    public function __construct(TranslatorInterface $translator)
+    private PortfolioService $portfolioService;
+
+    public function __construct(PortfolioService $portfolioService)
     {
-        $this->translator = $translator;
+        $this->portfolioService = $portfolioService;
     }
 
     #[Route('/switch-locale/{locale}', name: 'switch_locale', methods: ['POST', 'GET'])]
@@ -83,226 +84,10 @@ class HomeController extends AbstractController
         $today = new \DateTime();
         $age = $today->diff($birthDate)->y;
 
-        $skills = [
-            'Languages' => [
-                'icon' => 'fa-solid fa-code',
-                'color_bg' => 'blue-500/40',
-                'color_text' => 'blue-400',
-                'color_hover' => 'blue-800',
-                'items' => [
-                    [ 'icon' => 'fa-brands fa-html5', 'name' => 'HTML', 'display' => true ],
-                    [ 'icon' => 'fa-brands fa-css3-alt', 'name' => 'CSS', 'display' => true ],
-                    [ 'icon' => 'fa-brands fa-js', 'name' => 'JavaScript', 'display' => true ],
-                    [ 'image' => 'skills/typescript.svg', 'name' => 'TypeScript', 'display' => false ],
-                    [ 'icon' => 'fa-brands fa-php', 'name' => 'PHP', 'display' => true ],
-                    [ 'icon' => 'fa-brands fa-python', 'name' => 'Python', 'display' => true ],
-                    [ 'icon' => 'fa-brands fa-java', 'name' => 'Java', 'display' => true ],
-                    [ 'icon' => 'fa-brands fa-kotlin', 'name' => 'Kotlin', 'display' => false ],
-                    [ 'icon' => 'fa-brands fa-swift', 'name' => 'Swift', 'display' => false ]
-                ]
-            ],
-            'Frameworks' => [
-                'icon' => 'fa-solid fa-layer-group',
-                'color_bg' => 'red-500/40',
-                'color_text' => 'red-400',
-                'color_hover' => 'red-800',
-                'items' => [
-                    [ 'icon' => 'fa-brands fa-symfony', 'name' => 'Symfony', 'display' => true ],
-                    [ 'image' => 'skills/flask.svg', 'name' => 'Flask', 'display' => true ],
-                    [ 'image' => 'skills/next-js.svg', 'name' => 'Next.js', 'display' => false ],
-                    [ 'icon' => 'fa-brands fa-bootstrap', 'name' => 'Bootstrap', 'display' => true ],
-                    [ 'icon' => 'fa-solid fa-wind', 'name' => 'Tailwind CSS', 'display' => false ],
-                    // [ 'icon' => 'fa-solid fa-leaf', 'name' => 'Twig', 'display' => true ]
-                ]
-            ],
-            'Backend' => [
-                'icon' => 'fa-solid fa-server',
-                'color_bg' => 'green-500/40',
-                'color_text' => 'green-400',
-                'color_hover' => 'green-800',
-                'items' => [
-                    [ 'icon' => 'fa-brands fa-node', 'name' => 'Node.js', 'display' => true ],
-                    [ 'icon' => 'fa-solid fa-database', 'name' => 'MySQL', 'display' => true ],
-                    [ 'icon' => 'fa-solid fa-server', 'name' => 'REST APIs', 'display' => true ]
-                ]
-            ],
-            'Practices' => [
-                'icon' => 'fa-solid fa-list-check',
-                'color_bg' => 'yellow-500/40',
-                'color_text' => 'yellow-400',
-                'color_hover' => 'yellow-800',
-                'items' => [
-                    [ 'icon' => 'fa-solid fa-arrows-spin', 'name' => 'Agile', 'display' => true ],
-                    [ 'icon' => 'fa-brands fa-git-alt', 'name' => 'Git', 'display' => true ]
-                ]
-            ]
-        ];
-
-        $findTechByName = function($name) use ($skills) {
-            foreach ($skills as $category) {
-                foreach ($category['items'] as $item) {
-                    if ($item['name'] === $name) {
-                        return array_merge($item, [
-                            'color_bg' => $category['color_bg'],
-                            'color_text' => $category['color_text'],
-                            'color_hover' => $category['color_hover']
-                        ]);
-                    }
-                }
-            }
-            return null;
-        };
-
-        $experiences = [
-            [
-                'title' => $this->translator->trans('experiences.klimber_kids.title'),
-                'company' => 'Klimber-Kids',
-                'location' => $this->translator->trans('experiences.klimber_kids.location'),
-                'period' => $this->translator->trans('experiences.klimber_kids.period'),
-                'logo' => 'companies/klimber-kids.svg',
-                'description' => [
-                    $this->translator->trans('experiences.klimber_kids.description.1'),
-                    $this->translator->trans('experiences.klimber_kids.description.2'),
-                    $this->translator->trans('experiences.klimber_kids.description.3')
-                ],
-                'technologies' => [
-                    $findTechByName('PHP'),
-                    $findTechByName('HTML'),
-                    $findTechByName('CSS'),
-                    $findTechByName('JavaScript'),
-                    $findTechByName('Symfony'),
-                    $findTechByName('Bootstrap'),
-                    // $findTechByName('Twig'),
-                    $findTechByName('MySQL'),
-                    $findTechByName('Git'),
-                ]
-            ],
-            [
-                'title' => $this->translator->trans('experiences.cs_lane.title'),
-                'company' => 'CS-Lane',
-                'location' => $this->translator->trans('experiences.cs_lane.location'),
-                'period' => $this->translator->trans('experiences.cs_lane.period'),
-                'logo' => 'companies/cs-lane.png',
-                'description' => [
-                    $this->translator->trans('experiences.cs_lane.description.1'),
-                    $this->translator->trans('experiences.cs_lane.description.2'),
-                    $this->translator->trans('experiences.cs_lane.description.3'),
-                    $this->translator->trans('experiences.cs_lane.description.4')
-                ],
-                'technologies' => [
-                    $findTechByName('PHP'),
-                    $findTechByName('HTML'),
-                    $findTechByName('CSS'),
-                    $findTechByName('JavaScript'),
-                    $findTechByName('Kotlin'),
-                    $findTechByName('Swift'),
-                    $findTechByName('Bootstrap'),
-                    $findTechByName('MySQL'),
-                    $findTechByName('REST APIs'),
-                    $findTechByName('Agile'),
-                    $findTechByName('Git')
-                ]
-            ],
-            [
-                'title' => $this->translator->trans('experiences.uimm.title'),
-                'company' => 'UIMM Eure Seine Estuaire',
-                'location' => $this->translator->trans('experiences.uimm.location'),
-                'period' => $this->translator->trans('experiences.uimm.period'),
-                'logo' => 'companies/uimm.png',
-                'description' => [
-                    $this->translator->trans('experiences.uimm.description.1'),
-                    $this->translator->trans('experiences.uimm.description.2')
-                ],
-                'technologies' => [
-                    $findTechByName('Python'),
-                    $findTechByName('HTML'),
-                    $findTechByName('CSS'),
-                    $findTechByName('JavaScript'),
-                    $findTechByName('Flask'),
-                    $findTechByName('Bootstrap'),
-                    $findTechByName('REST APIs'),
-                ]
-            ]
-        ];
-
-        $educations = [
-            [
-                'degree' => $this->translator->trans('education.bts.degree'),
-                'school' => 'Lycée Gustave Flaubert',
-                'location' => $this->translator->trans('education.bts.location'),
-                'period' => $this->translator->trans('education.bts.period'),
-                'logo' => 'schools/gustave-flaubert.png',
-                'description' => [
-                    $this->translator->trans('education.bts.description.1'),
-                ]
-            ],
-            [
-                'degree' => $this->translator->trans('education.bac.degree'),
-                'school' => 'Lycée Aristide Briand',
-                'location' => $this->translator->trans('education.bac.location'),
-                'period' => $this->translator->trans('education.bac.period'),
-                'logo' => 'schools/aristide-briand.webp',
-                'description' => [
-                    $this->translator->trans('education.bac.description.1'),
-                ]
-            ]
-        ];
-
-        $projects = [
-            [
-                'title' => $this->translator->trans('projects.klimber_kids.title'),
-                'description' => $this->translator->trans('projects.klimber_kids.description'),
-                'image' => 'projects/klimber-kids.png',
-                'github' => null,
-                'website' => 'https://klimber-kids.com',
-                'demo' => null,
-                'technologies' => [
-                    $findTechByName('PHP'),
-                    $findTechByName('HTML'),
-                    $findTechByName('CSS'),
-                    $findTechByName('JavaScript'),
-                    $findTechByName('Symfony'),
-                    $findTechByName('Bootstrap'),
-                    // $findTechByName('Twig'),
-                    $findTechByName('MySQL'),
-                    $findTechByName('Git'),
-                ]
-                ],
-                [
-                    'title' => $this->translator->trans('projects.dashboard-si.title'),
-                    'description' => $this->translator->trans('projects.dashboard-si.description'),
-                    'image' => 'projects/dashboard-si.png',
-                    'github' => 'https://github.com/Will6855/IT-Department-Dashboard',
-                    'website' => null,
-                    'demo' => 'https://it-department-dashboard-demo.vercel.app/',
-                    'technologies' => [
-                        $findTechByName('Python'),
-                        $findTechByName('HTML'),
-                        $findTechByName('CSS'),
-                        $findTechByName('JavaScript'),
-                        $findTechByName('Flask'),
-                        $findTechByName('REST APIs'),
-                    ]
-                ],
-                [
-                    'title' => $this->translator->trans('projects.email-sender.title'),
-                    'description' => $this->translator->trans('projects.email-sender.description'),
-                    'image' => 'projects/email-sender.png',
-                    'github' => 'https://github.com/Will6855/HTML-Email-Sender',
-                    'website' => null,
-                    'demo' => '',
-                    'technologies' => [
-                        $findTechByName('HTML'),
-                        $findTechByName('CSS'),
-                        $findTechByName('JavaScript'),
-                        $findTechByName('TypeScript'),
-                        $findTechByName('Next.js'),
-                        $findTechByName('Tailwind CSS'),
-                        $findTechByName('Node.js'),
-                        $findTechByName('Git'),
-                    ]
-                ],
-        ];
+        $skills = $this->portfolioService->getSkills();
+        $experiences = $this->portfolioService->getExperiences();
+        $educations = $this->portfolioService->getEducations();
+        $projects = $this->portfolioService->getProjects();
 
         return $this->render('home/index.html.twig', [
             'skills' => $skills,
---
+++ src/Service/PortfolioService.php
@@ -0,0 +1,192 @@
+<?php
+// src/Service/PortfolioService.php
+namespace App\Service;
+
+class PortfolioService
+{
+    public function getSkills(): array
+    {
+        return [
+            'Languages' => [
+                'icon' => 'fa-solid fa-code',
+                'color_bg' => 'blue-500/40',
+                'color_text' => 'blue-400',
+                'color_hover' => 'blue-800',
+                'items' => [
+                    [ 'icon' => 'fa-brands fa-html5', 'name' => 'HTML', 'display' => true ],
+                    [ 'icon' => 'fa-brands fa-css3-alt', 'name' => 'CSS', 'display' => true ],
+                    [ 'icon' => 'fa-brands fa-js', 'name' => 'JavaScript', 'display' => true ],
+                    [ 'image' => 'skills/typescript.svg', 'name' => 'TypeScript', 'display' => false ],
+                    [ 'icon' => 'fa-brands fa-php', 'name' => 'PHP', 'display' => true ],
+                    [ 'icon' => 'fa-brands fa-python', 'name' => 'Python', 'display' => true ],
+                    [ 'icon' => 'fa-brands fa-java', 'name' => 'Java', 'display' => true ],
+                    [ 'icon' => 'fa-brands fa-kotlin', 'name' => 'Kotlin', 'display' => false ],
+                    [ 'icon' => 'fa-brands fa-swift', 'name' => 'Swift', 'display' => false ]
+                ]
+            ],
+            'Frameworks' => [
+                'icon' => 'fa-solid fa-layer-group',
+                'color_bg' => 'red-500/40',
+                'color_text' => 'red-400',
+                'color_hover' => 'red-800',
+                'items' => [
+                    [ 'icon' => 'fa-brands fa-symfony', 'name' => 'Symfony', 'display' => true ],
+                    [ 'image' => 'skills/flask.svg', 'name' => 'Flask', 'display' => true ],
+                    [ 'image' => 'skills/next-js.svg', 'name' => 'Next.js', 'display' => false ],
+                    [ 'icon' => 'fa-brands fa-bootstrap', 'name' => 'Bootstrap', 'display' => true ],
+                    [ 'icon' => 'fa-solid fa-wind', 'name' => 'Tailwind CSS', 'display' => false ],
+                    // [ 'icon' => 'fa-solid fa-leaf', 'name' => 'Twig', 'display' => true ]
+                ]
+            ],
+            'Backend' => [
+                'icon' => 'fa-solid fa-server',
+                'color_bg' => 'green-500/40',
+                'color_text' => 'green-400',
+                'color_hover' => 'green-800',
+                'items' => [
+                    [ 'icon' => 'fa-brands fa-node', 'name' => 'Node.js', 'display' => true ],
+                    [ 'icon' => 'fa-solid fa-database', 'name' => 'MySQL', 'display' => true ],
+                    [ 'icon' => 'fa-solid fa-server', 'name' => 'REST APIs', 'display' => true ]
+                ]
+            ],
+            'Practices' => [
+                'icon' => 'fa-solid fa-list-check',
+                'color_bg' => 'yellow-500/40',
+                'color_text' => 'yellow-400',
+                'color_hover' => 'yellow-800',
+                'items' => [
+                    [ 'icon' => 'fa-solid fa-arrows-spin', 'name' => 'Agile', 'display' => true ],
+                    [ 'icon' => 'fa-brands fa-git-alt', 'name' => 'Git', 'display' => true ]
+                ]
+            ]
+        ];
+    }
+
+    public function findTechByName(string $name): ?array
+    {
+        $skills = $this->getSkills();
+        
+        foreach ($skills as $category) {
+            foreach ($category['items'] as $item) {
+                if ($item['name'] === $name) {
+                    return array_merge($item, [
+                        'color_bg' => $category['color_bg'],
+                        'color_text' => $category['color_text'],
+                        'color_hover' => $category['color_hover']
+                    ]);
+                }
+            }
+        }
+        return null;
+    }
+
+    public function getExperiences(): array
+    {
+        return [
+            [
+                'title' => 'experiences.klimber_kids.title',
+                'company' => 'Klimber-Kids',
+                'location' => 'experiences.klimber_kids.location',
+                'period' => 'experiences.klimber_kids.period',
+                'logo' => 'companies/klimber-kids.svg',
+                'description' => [
+                    'experiences.klimber_kids.description.1',
+                    'experiences.klimber_kids.description.2',
+                    'experiences.klimber_kids.description.3'
+                ],
+                'technologies' => array_map(fn($tech) => $this->findTechByName($tech), 
+                    ['PHP', 'HTML', 'CSS', 'JavaScript', 'Symfony', 'Bootstrap', 'MySQL', 'Git'])
+            ],
+            [
+                'title' => 'experiences.cs_lane.title',
+                'company' => 'CS-Lane',
+                'location' => 'experiences.cs_lane.location',
+                'period' => 'experiences.cs_lane.period',
+                'logo' => 'companies/cs-lane.png',
+                'description' => [
+                    'experiences.cs_lane.description.1',
+                    'experiences.cs_lane.description.2',
+                    'experiences.cs_lane.description.3',
+                    'experiences.cs_lane.description.4'
+                ],
+                'technologies' => array_map(fn($tech) => $this->findTechByName($tech), 
+                    ['PHP', 'HTML', 'CSS', 'JavaScript', 'Kotlin', 'Swift', 'Bootstrap', 'MySQL', 'REST APIs', 'Agile', 'Git'])
+            ],
+            [
+                'title' => 'experiences.uimm.title',
+                'company' => 'UIMM Eure Seine Estuaire',
+                'location' => 'experiences.uimm.location',
+                'period' => 'experiences.uimm.period',
+                'logo' => 'companies/uimm.png',
+                'description' => [
+                    'experiences.uimm.description.1',
+                    'experiences.uimm.description.2'
+                ],
+                'technologies' => array_map(fn($tech) => $this->findTechByName($tech), 
+                    ['Python', 'HTML', 'CSS', 'JavaScript', 'Flask', 'Bootstrap', 'REST APIs'])
+            ]
+        ];
+    }
+
+    public function getEducations(): array
+    {
+        return [
+            [
+                'degree' => 'education.bts.degree',
+                'school' => 'Lycée Gustave Flaubert',
+                'location' => 'education.bts.location',
+                'period' => 'education.bts.period',
+                'logo' => 'schools/gustave-flaubert.png',
+                'description' => [
+                    'education.bts.description.1',
+                ]
+            ],
+            [
+                'degree' => 'education.bac.degree',
+                'school' => 'Lycée Aristide Briand',
+                'location' => 'education.bac.location',
+                'period' => 'education.bac.period',
+                'logo' => 'schools/aristide-briand.webp',
+                'description' => [
+                    'education.bac.description.1',
+                ]
+            ]
+        ];
+    }
+
+    public function getProjects(): array
+    {
+        return [
+            [
+                'title' => 'projects.klimber_kids.title',
+                'description' => 'projects.klimber_kids.description',
+                'image' => 'projects/klimber-kids.png',
+                'github' => null,
+                'website' => 'https://klimber-kids.com',
+                'demo' => null,
+                'technologies' => array_map(fn($tech) => $this->findTechByName($tech), 
+                    ['PHP', 'HTML', 'CSS', 'JavaScript', 'Symfony', 'Bootstrap', 'MySQL', 'Git'])
+            ],
+            [
+                'title' => 'projects.dashboard-si.title',
+                'description' => 'projects.dashboard-si.description',
+                'image' => 'projects/dashboard-si.png',
+                'github' => 'https://github.com/Will6855/IT-Department-Dashboard',
+                'website' => null,
+                'demo' => 'https://it-department-dashboard-demo.vercel.app/',
+                'technologies' => array_map(fn($tech) => $this->findTechByName($tech), 
+                    ['Python', 'HTML', 'CSS', 'JavaScript', 'Flask', 'REST APIs'])
+            ],
+            [
+                'title' => 'projects.email-sender.title',
+                'description' => 'projects.email-sender.description',
+                'image' => 'projects/email-sender.png',
+                'github' => 'https://github.com/Will6855/HTML-Email-Sender',
+                'website' => null,
+                'demo' => '',
+                'technologies' => array_map(fn($tech) => $this->findTechByName($tech), 
+                    ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Next.js', 'Tailwind CSS', 'Node.js', 'Git'])
+            ],
+        ];
+    }
+}
